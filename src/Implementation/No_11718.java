6d00f7d91d1aac302fb788c9c8e2a6e51136cb20
alkwen0996 <alkwen0996@naver.com>
2021-03-01 11:17:43 +0900

    commit No_11720

---
+++ src/Implementation/No_11718.java
@@ -0,0 +1,21 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class No_11718 {
+    public static void main(String[] args){
+
+        String str = "";
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            str = sc.nextLine();
+            System.out.println(str);
+            if(str.isEmpty()){
+                break;
+            }
+        }
+        sc.close();
+
+    }// main
+}// class
