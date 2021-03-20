efed01c07e73a883a266a0ef3c8896f44a632f7c
alkwen0996 <alkwen0996@naver.com>
2021-03-20 19:21:09 +0900

    commit and push revise No_1264

--- src/Implementation/No_2309.java
+++ src/Implementation/No_2309.java
@@ -7,20 +7,21 @@ import java.util.Arrays;
 
 public class No_2309 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final int arrLeg = 9;
     public static void main(String args[])throws IOException {
 
-        int [] arr = new int[9];
-        int tmp,sum =0;
+        int [] arr = new int[arrLeg];
+        int tmp,sum=0;
 
-        for (int i =0;i<9;i++){
+        for (int i =0;i<arrLeg;i++){
             arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }// for
 
         int x=0,y=0;
         out:
-        for (int i =0;i<arr.length-1;i++){
-            for (int j =i+1; j<arr.length;j++){
+        for (int i =0;i<arrLeg-1;i++){
+            for (int j =i+1; j<arrLeg;j++){
                 tmp = sum-(arr[i]+arr[j]);
                 if(tmp == 100){
                     x = arr[i];
@@ -32,7 +33,7 @@ public class No_2309 {
 
         Arrays.sort(arr);
 
-        for (int i =0; i<arr.length;i++){
+        for (int i =0; i<arrLeg;i++){
             if(arr[i] != x && arr[i] != y){
                 System.out.println(arr[i]);
             }
