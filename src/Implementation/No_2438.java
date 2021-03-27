88df4f6bf9ff192aeb04fb9907000b2e8d1de9dd
alkwen0996 <alkwen0996@naver.com>
2021-03-27 21:34:42 +0900

    commit and push No_2438,No_2475

---
+++ src/Implementation/No_2438.java
@@ -0,0 +1,17 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class No_2438 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
+        int lineCnt = sc.nextInt();
+
+        for(int i=0;i<lineCnt;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
