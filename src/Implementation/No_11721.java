a2c5fa47de65b0e63cadc8260db98692de23ec0d
alkwen0996 <alkwen0996@naver.com>
2021-05-28 14:50:31 +0900

    add No_11721

--- src/Implementation/No_11721.java
+++ src/Implementation/No_11721.java
@@ -1,6 +1,5 @@
 package Implementation;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class No_11721 {
@@ -9,39 +8,31 @@ public class No_11721 {
     public static void main(String[] args) {
         String input = inputData();
         int StringLength = input.length();
-        System.out.println("StringLength: "+StringLength);
-        int printCount = StringLength / 10;
-        System.out.println("printCount: "+printCount);
-        int lastLineLength = StringLength % 10;
-        System.out.println("lastLineLength: "+lastLineLength);
 
-        if (StringLength < 10)
+        if (StringLength <= 10)
             System.out.println(input);
 
-        for (int i = 0 ; i <= printCount ; i++){
-            if(lastLineLength==0)
-                System.out.println(input.substring(i,i+10));
-            if(lastLineLength>0)
-                System.out.println(input.substring(i,i+10));
-            if(input.length()<10)
-                System.out.println(input);
-
-            input = input.substring(i+10);
-            System.out.println("input: "+input);
+        if (StringLength > 10) {
+            int idx = 0;
+            while (StringLength > 10) {
+                System.out.println(input.substring(idx, idx + 10));
+                input = input.substring(idx + 10, StringLength);
+                StringLength -= 10;
+            }
+            System.out.println(input);
         }
-
     }
 
     private static String inputData() {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         boolean flag = input.matches(PATTERN);
-        if (!flag && input.length() < 100 && input.length() > 0) {
+        if (!flag && input.length() <= 100 && input.length() > 0) {
             try {
                 throw new Exception("영어 대소문자만 입력 가능합니다.");
             } catch (Exception e) {
                 e.printStackTrace();
-                inputData();
+                input = inputData();
             }
         }
         return input;
