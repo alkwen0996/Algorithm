57f7a8aedec9498444fef599225707122096f54c
minsulee <alkwen0996@naver.com>
2021-06-13 16:38:30 +0900

    add No_11721 DP_Lis

---
+++ src/Implementation/No_9095.java
@@ -0,0 +1,25 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_9095 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++){
+            int result = methodNumber(Integer.parseInt(br.readLine()));
+            System.out.println(result);
+        }
+    }
+
+    private static int methodNumber(int number) {
+        int result =0;
+
+
+
+        return result;
+    }
+}
