4d961c5ddcb6df67433f0eb4e7c39a7068ff4ad0
minsulee <alkwen0996@naver.com>
2021-08-17 20:34:10 +0900

    add Loop_While Solve problems (No_10951,No_10952)

---
+++ src/Loop_While/No_10951.java
@@ -0,0 +1,22 @@
+package Loop_While;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = "";
+        while((input=bufferedReader.readLine()) != null){
+            StringTokenizer stringTokenizer = new StringTokenizer(input," ");
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(a+b);
+        }
+
+    }
+}
---
+++ src/Loop_While/No_10952.java
@@ -0,0 +1,22 @@
+package Loop_While;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()," ");
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            if(a+b==0){
+                break;
+            }
+            System.out.println(a+b);
+        }
+    }
+}
