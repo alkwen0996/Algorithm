efec50cec3f428c5613ca614ca2c21bfd996b261
minsulee <alkwen0996@naver.com>
2021-08-17 23:00:58 +0900

    add Loop_While new explanation (No_1110)

--- src/Loop_While/No_1110.java
+++ src/Loop_While/No_1110.java
@@ -3,60 +3,22 @@ package Loop_While;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
 public class No_1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringbuilder = new StringBuilder();
-        String input = "";
 
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-
-        input = stringTokenizer.nextToken();
-        String originalInput = input;
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int copy = N;
         int count = 0;
 
-        if(originalInput.length() == 1){
-            originalInput = "0"+originalInput;
-        }
+        do {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            count++;
+        } while (copy != N);
 
-        count = getCount(stringbuilder, input, originalInput, count);
         System.out.println(count);
 
-    }
-
-    private static int getCount(StringBuilder stringbuilder, String input, String originalInput, int count) {
-        while (!stringbuilder.toString().equals(originalInput.toString())) {
-
-            if (stringbuilder.toString().length() != 0) {
-                input = stringbuilder.toString();
-                stringbuilder.delete(0, stringbuilder.length());
-            }
-
-            if (input.length() == 1) {
-                stringbuilder.append("0").append(input);
-            }
-
-            if (input.length() == 2) {
-                stringbuilder.append(input);
-            }
-
-            String tensDigit = stringbuilder.substring(0, 1);
-            String unitDigit = stringbuilder.substring(1, 2);
-            stringbuilder.delete(0, stringbuilder.length());
-
-            int sum = Integer.parseInt(tensDigit) + Integer.parseInt(unitDigit);
-
-            stringbuilder.append(unitDigit).append(sum);
-
-            if (stringbuilder.toString().length() == 3) {
-                stringbuilder.delete(1, 2);
-            }
-
-            count++;
-        }
-        return count;
     }
 }
