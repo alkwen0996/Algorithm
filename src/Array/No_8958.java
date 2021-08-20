ff0c5552e01b121aa50f4e960facbd834a2f1c02
minsulee <alkwen0996@naver.com>
2021-08-20 18:11:52 +0900

    add Array Solve Problem (No_8958)

---
+++ src/Array/No_8958.java
@@ -0,0 +1,52 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int lineCount = Integer.parseInt(bufferedReader.readLine());
+
+        String [] inputList = new String[lineCount];
+        for (int i = 0; i < lineCount; i++ ){
+            inputList[i] = bufferedReader.readLine();
+        }
+
+        int [] result = createScoreList(inputList);
+
+        for (int j : result) {
+            System.out.println(j);
+        }
+    }
+
+    private static int [] createScoreList(String[] inputList) {
+
+        int [] scores = new int [inputList.length];
+        for (int i = 0; i < inputList.length ; i++ ){
+            char [] oxList = inputList[i].toCharArray();
+            scores[i] = calculateScore(oxList);
+        }
+
+        return scores;
+    }
+
+    private static int calculateScore(char[] oxList) {
+        int sum = 0;
+        int count = 0;
+
+        for (char c : oxList) {
+            if (c == 'O') {
+                count++;
+                sum += count;
+            }
+            if (c == 'X') {
+                count = 0;
+            }
+        }
+
+        return sum;
+    }
+}
