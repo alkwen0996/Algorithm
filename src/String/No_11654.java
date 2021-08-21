623651047651c5a4d11c148a0bf4aae798f6f360
minsulee <alkwen0996@naver.com>
2021-08-21 23:52:00 +0900

    add Functions Solve Problems (No_11720, No_11654)

---
+++ src/String/No_11654.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
+
+        int inputChar = input.charAt(0);
+        System.out.println(inputChar);
+    }
+}
---
+++ src/String/No_11720.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+
+        String inputNumberString = bufferedReader.readLine();
+        List<Integer> inputNumbers = new ArrayList<>();
+
+        char [] inputNumbersChar = inputNumberString.toCharArray();
+        for (char c : inputNumbersChar) {
+            inputNumbers.add(Integer.parseInt(String.valueOf(c)));
+        }
+
+        System.out.println(inputNumbers.stream().mapToInt(Integer::intValue).sum());
+
+    }
+}
