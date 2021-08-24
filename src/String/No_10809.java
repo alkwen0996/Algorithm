c378fab0d63bce741dfee29db39f5990a2b5e210
minsulee <alkwen0996@naver.com>
2021-08-24 21:44:09 +0900

    add String another solution (No_10809)

--- src/String/No_10809.java
+++ src/String/No_10809.java
@@ -3,39 +3,16 @@ package String;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class No_10809 {
-    public static final int ALPHABET_COUNT = 26;
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String input = bufferedReader.readLine();
 
-        char[] inputCharsArray = input.toCharArray();
-        String result = solve(inputCharsArray);
-        System.out.println(result);
-    }
-
-    private static String solve(char[] inputCharsArray) {
-        List<Character> inputChars = new ArrayList<>();
-
-        for (char c : inputCharsArray) {
-            inputChars.add(c);
-        }
-
         StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < ALPHABET_COUNT; i++) {
-            if (!inputChars.contains((char) (97 + i))) {
-                stringBuilder.append(-1).append(" ");
-            }
-            if (inputChars.contains((char) (97 + i))) {
-                stringBuilder.append(inputChars.indexOf((char) (97 + i))).append(" ");
-            }
+        for (char c = 'a'; c <= 'z'; c++){
+            stringBuilder.append(input.indexOf(c)).append(" ");
         }
-
-        return stringBuilder.toString();
+            System.out.print(stringBuilder.toString());
     }
 }
