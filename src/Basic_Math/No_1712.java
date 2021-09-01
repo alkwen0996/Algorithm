ded75f596079ce45b2f4e1072a8f7f53f50a12e8
minsulee <alkwen0996@naver.com>
2021-09-01 23:52:35 +0900

    add Basic_Math Code Refactoring (No_1712)

--- src/Basic_Math/No_1712.java
+++ src/Basic_Math/No_1712.java
@@ -3,29 +3,24 @@ package Basic_Math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class No_1712 {
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int count = 1;
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        int fixedCost = bufferedReader.read();
-        int variableCost = bufferedReader.read();
-        int notebookCost = bufferedReader.read();
-        System.out.println(solve(fixedCost, variableCost, notebookCost, count));
+        int fixedCost = Integer.parseInt(stringTokenizer.nextToken());
+        int variableCost = Integer.parseInt(stringTokenizer.nextToken());
+        int notebookCost = Integer.parseInt(stringTokenizer.nextToken());
+        System.out.println(solve(fixedCost, variableCost, notebookCost));
     }
 
-    private static int solve(int fixedCost, int variableCost, int notebookCost, int count) {
-        if (variableCost > notebookCost) {
+    private static int solve(int fixedCost, int variableCost, int notebookCost) {
+        if (variableCost >= notebookCost) {
             return -1;
         }
-//      while (fixedCost + (variableCost * count) >= notebookCost * count) {
-        if (fixedCost / (notebookCost - variableCost) > count) {
-            count++;
-            return solve(fixedCost, variableCost, notebookCost, count);
-        } else{
-            return count;
-        }
 
+      return fixedCost / (notebookCost - variableCost)+1;
     }
 }
