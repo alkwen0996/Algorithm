27a5eb9cd9e344758cc4079c956c78b122d1738d
minsulee <alkwen0996@naver.com>
2021-09-08 23:49:48 +0900

    add Basic_Math Code Refactoring (No_10250)

--- src/Basic_Math/No_10250.java
+++ src/Basic_Math/No_10250.java
@@ -6,14 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class No_10250 {
-    public static final int START_ROOM_NO = 101;
-    public static final int HUNDRED = 100;
-    public static final int ONE = 1;
-
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int inputTestCase = Integer.parseInt(bufferedReader.readLine());
-        System.out.println(inputTestCase);
         final StringBuilder stringBuilder = new StringBuilder();
 
         for (int i = 0; i < inputTestCase; i++) {
@@ -27,22 +22,15 @@ public class No_10250 {
         int H = Integer.parseInt(stringTokenizer.nextToken());
         int W = Integer.parseInt(stringTokenizer.nextToken());
         int N = Integer.parseInt(stringTokenizer.nextToken());
-        System.out.println("H: " + H + " W: " + W + " N: " + N);
 
-        int roomNo = START_ROOM_NO; // 101
-        int count = ONE; // 1
-        int roomHeight = H * HUNDRED; // 6 * 100, 30 * 100
-        System.out.println("roomHeight: " + roomHeight);
+        int x = (N / H) + 1;
+        int y = N % H;
 
-        while(count+1 < N){
-            if (roomNo / roomHeight == ONE) {
-                roomNo = roomNo - ((H - ONE) * HUNDRED) + ONE;
-            }
-            System.out.println("roomNo: " + roomNo + " count:" + count + " N: " + N);
-            roomNo += HUNDRED;
-            count++;
+        if (N % H == 0) {
+            x = N / H;
+            y = H;
         }
 
-        return roomNo;
+        return (y * 100 + x);
     }
 }
