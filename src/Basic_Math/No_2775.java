ec8fbdc5e8ab011ea24d5df2f62bbc4d89cd50e4
minsulee <alkwen0996@naver.com>
2021-09-11 17:05:18 +0900

    add Basic_Math Commit (No_2775)

--- src/Basic_Math/No_2775.java
+++ src/Basic_Math/No_2775.java
@@ -13,21 +13,12 @@ public class No_2775 {
         for (int i = 0; i < lineCount; i++) {
             int k = Integer.parseInt(bufferedReader.readLine());
             int n = Integer.parseInt(bufferedReader.readLine());
-            int result = solve(k, n);
-            stringBuilder.append(result).append("\n");
+            stringBuilder.append(solve(k, n)).append("\n");
         }
-
         System.out.println(stringBuilder);
-
     }
 
     private static int solve(final int k, final int n) {
-        // a층 b호에 살려면
-        // a-1층 1~b호 사람들 수를 합한것만큼 사람들을 데려와 살아야함.
-        // k층 n호에 몇명이 사는지 출력.
-        // 아파트는 0층부터 있고 1호부터 있다.
-        // 0층의 i호에는 i명이 산다.
-        // 각 층 1호 = 1 / 2호 k + 2 /
         if (k == 0) {
             return n;
         }
