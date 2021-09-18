c293d953257c3c03855380b535f925752b3f9f5b
minsulee <alkwen0996@naver.com>
2021-09-18 11:02:47 +0900

    add Basic_Math2 Commit (Second Solve No_11653)

--- src/BasicMath2/No_11653.java
+++ src/BasicMath2/No_11653.java
@@ -3,8 +3,6 @@ package BasicMath2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class No_11653 {
     public static void main(String[] args) throws IOException {
@@ -15,47 +13,19 @@ public class No_11653 {
         System.out.println(result);
     }
 
-    private static String solve(final int N) {
-        if (N == 1) {
-            return "";
-        }
-
-        List<Integer> factors = new ArrayList<>();
-        for (int i = 2; i <= N; i++) {
-            if (N % i == 0) {
-                if (isPrimeFactor(i)) {
-                    factors.add(i);
-                }
-            }
-        }
-
-        return factorization(N, factors);
-    }
-
-    private static String factorization(int N, final List<Integer> factors) {
+    private static String solve(int N) {
         final StringBuilder stringBuilder = new StringBuilder();
-        int index = 0;
-        int share = factors.get(0);
-        while (!isPrimeFactor(N)) {
-            if (N % share != 0) {
-                share = factors.get(index++);
-            }
-            if (N % share == 0) {
-                stringBuilder.append(share).append("\n");
-                N = N / share;
+
+        int x = 2;
+        while (N != 1) {
+            if (N % x == 0) {
+                stringBuilder.append(x).append("\n");
+                N /= x;
+            }else{
+                x++;
             }
         }
-        stringBuilder.append(N).append("\n");
 
         return stringBuilder.toString();
-    }
-
-    private static boolean isPrimeFactor(final Integer factor) {
-        for (int i = 2; i < factor; i++) {
-            if (factor % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
