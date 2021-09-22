672bfd492fedf417c4363d3813af46bd0ebb7e7d
minsulee <alkwen0996@naver.com>
2021-09-22 12:47:26 +0900

    add Data_Structure Commit (Second Solve_UsingStack No_9012)

--- src/DataStructure/No_9012.java
+++ src/DataStructure/No_9012.java
@@ -3,40 +3,46 @@ package DataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class No_9012 {
+
+    public static final String NEW_LINE = "\n";
+    public static final String REGEX = "";
+    public static final String NO = "NO";
+    public static final String YES = "YES";
+
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         final StringBuilder stringBuilder = new StringBuilder();
         int T = Integer.parseInt(bufferedReader.readLine());
 
         for (int i = 0; i < T; i++) {
-            stringBuilder.append(solve(bufferedReader.readLine())).append("\n");
+            stringBuilder.append(solve(bufferedReader.readLine())).append(NEW_LINE);
         }
 
         System.out.println(stringBuilder.toString());
     }
 
     private static String solve(final String inputData) {
-        final char[] parentheses = inputData.toCharArray();
+        final String[] parentheses = inputData.split(REGEX);
+        final Stack<String> checkVPS = new Stack<>();
+        String result = NO;
 
-        int VPSFlag = 0;
-        for (final char parenthesis : parentheses) {
-            if (parenthesis == '(') {
-                VPSFlag++;
-            }
-            if (parenthesis == ')') {
-                VPSFlag--;
-            }
-            if (VPSFlag < 0) {
-                return "NO";
+        for (final String parenthesis : parentheses) {
+            if (parenthesis.equals("(")) {
+                checkVPS.push(parenthesis);
+            } else if (parenthesis.equals(")") && !checkVPS.isEmpty()) {
+                checkVPS.pop();
+            } else {
+                return NO;
             }
         }
 
-        if (VPSFlag == 0) {
-            return "YES";
+        if (checkVPS.isEmpty()) {
+            result = YES;
         }
 
-        return "NO";
+        return result;
     }
 }
