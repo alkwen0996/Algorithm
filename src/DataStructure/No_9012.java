4c5e729a101c44bd4ae239713e71ed5d4377ece3
minsulee <alkwen0996@naver.com>
2021-09-23 09:11:26 +0900

    add Data_Structure Commit (No_9012) use toCharArray and if~else

--- src/DataStructure/No_9012.java
+++ src/DataStructure/No_9012.java
@@ -6,9 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class No_9012 {
-
     public static final String NEW_LINE = "\n";
-    public static final String REGEX = "";
     public static final String NO = "NO";
     public static final String YES = "YES";
 
@@ -25,14 +23,14 @@ public class No_9012 {
     }
 
     private static String solve(final String inputData) {
-        final String[] parentheses = inputData.split(REGEX);
-        final Stack<String> checkVPS = new Stack<>();
+        final char[] parentheses = inputData.toCharArray();
+        final Stack<Character> checkVPS = new Stack<>();
         String result = NO;
 
-        for (final String parenthesis : parentheses) {
-            if (parenthesis.equals("(")) {
+        for (final char parenthesis : parentheses) {
+            if (parenthesis == '(') {
                 checkVPS.push(parenthesis);
-            } else if (parenthesis.equals(")") && !checkVPS.isEmpty()) {
+            } else if (parenthesis == ')' && !checkVPS.isEmpty()) {
                 checkVPS.pop();
             } else {
                 return NO;
@@ -42,7 +40,6 @@ public class No_9012 {
         if (checkVPS.isEmpty()) {
             result = YES;
         }
-
         return result;
     }
 }
