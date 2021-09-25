2b528fe96d01b69c7c74453c574e475e818233f5
minsulee <alkwen0996@naver.com>
2021-09-25 11:11:49 +0900

    add Data_Structure Commit (No_5397)

--- src/DataStructure/No_5397.java
+++ src/DataStructure/No_5397.java
@@ -5,9 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-
+import java.util.LinkedList;
+import java.util.List;
 
 public class No_5397 {
+
+    public static final String DASH = "-";
+    public static final String LEFT_INEQUALITY_SIGN = ">";
+    public static final String RIGHT_INEQUALITY_SIGN = "<";
+
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         final StringBuilder stringBuilder = new StringBuilder();
@@ -24,13 +30,37 @@ public class No_5397 {
         // 화살표는 < or >
         // 커서의 위치를 움직일 수 있다면 왼쪽 또는 오른쪽으로 1만큼 이동.
         // 링크드리스트 & 스택이용
-        final Deque<String> password = new ArrayDeque<>();
-        char[] inputData = readLine.toCharArray();
 
-        for (int i = 0; i < inputData.length; i++) {
-
+        String result = readLine;
+        if (readLine.contains(RIGHT_INEQUALITY_SIGN) || readLine.contains(LEFT_INEQUALITY_SIGN) || readLine.contains(DASH)) {
+            result = deleteSign(readLine);
         }
 
-        return "";
+        return result;
+    }
+
+    private static String deleteSign(final String readLine) {
+        final List<String> resultPW = new LinkedList<>();
+        final Deque<String> inputPasswords = new ArrayDeque<>();
+        char[] inputDataChars = readLine.toCharArray();
+
+        for (final char inputDataChar : inputDataChars) {
+            inputPasswords.push(String.valueOf(inputDataChar));
+        }
+
+        for (int i = 0; i < inputPasswords.size(); i++) {
+            String word = inputPasswords.pop();
+            if(word.equals(DASH)){
+                inputPasswords.pop();
+            }else if(word.equals(RIGHT_INEQUALITY_SIGN)){
+
+            }else if(word.equals(LEFT_INEQUALITY_SIGN)){
+
+            }else{
+
+            }
+        }
+
+        return null;
     }
 }
