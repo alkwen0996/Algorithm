69084bb333f3be87f80e46cc1d3e709751eda20b
minsulee <alkwen0996@naver.com>
2021-10-02 00:00:24 +0900

    add Basic_Math Commit (Solve No_1550)

--- src/Basic_Math/No_1550.java
+++ src/Basic_Math/No_1550.java
@@ -7,7 +7,9 @@ import java.io.InputStreamReader;
 public class No_1550 {
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(bufferedReader.readLine());
+        String input = bufferedReader.readLine();
 
+        int decimal = Integer.parseInt(input, 16);
+        System.out.println(decimal);
     }
 }
