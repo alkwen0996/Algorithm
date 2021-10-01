2125cdff4fdf848ec1eacc8037f1ce1eff8bea86
minsulee <alkwen0996@naver.com>
2021-10-01 23:58:16 +0900

    add Data_Structure Commit (No_1550)

---
+++ src/Basic_Math/No_1550.java
@@ -0,0 +1,13 @@
+package Basic_Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1550 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(bufferedReader.readLine());
+
+    }
+}
