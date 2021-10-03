3c067733e020cb4c471f08aff60999090cf14054
minsulee <alkwen0996@naver.com>
2021-10-04 02:04:34 +0900

    add Basic_Math2 Commit (Add Comments No_3009)

--- src/DataStructure/No_5397.java
+++ src/DataStructure/No_5397.java
@@ -1,14 +1,14 @@
 package DataStructure;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+// 전반적으로 문제 이해를 잘못하고 품.
 public class No_5397 {
     public static final String DASH = "-";
     public static final String LEFT_INEQUALITY_SIGN = "<";
     public static final String RIGHT_INEQUALITY_SIGN = ">";
+    public static final int ZERO = 0;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +18,7 @@ public class No_5397 {
         for (int i = 0; i < lineCount; i++) {
             stringBuilder.append(solve(bufferedReader.readLine())).append("\n");
         }
+
         System.out.println(stringBuilder.toString());
     }
 
@@ -25,7 +26,7 @@ public class No_5397 {
         String result = readLine;
         if (readLine.contains(RIGHT_INEQUALITY_SIGN) || readLine.contains(LEFT_INEQUALITY_SIGN) || readLine.contains(DASH)) {
             result = deleteSign(readLine);
-        }
+        } // 화살표나 "-" 기호를 입력문자열이 포함하고 있으면 Password 구하는 함수(deleteSign)로 이동
 
         return result;
     }
@@ -33,37 +34,46 @@ public class No_5397 {
     private static String deleteSign(final String readLine) {
         char[] inputDataCharters = readLine.toCharArray();
         final Deque<String> inputPassword = new ArrayDeque<>();
-        final List<String> resultPassword = new LinkedList<>();
+        final StringBuilder stringBuilder = new StringBuilder();
 
+//        final ArrayList<Object> objects = new ArrayList<>();
+//        for (char inputDataCharter : inputDataCharters) {
+//            objects.add(inputDataCharter);
+//        }
+//        System.out.println(objects.toString());
+//        System.out.println("-------------------------------");
+
+
+        boolean leftChangeLocationFlag = true; // 왼쪽으로 커서 이동 플래그
         for (char inputDataCharter : inputDataCharters) {
-            inputPassword.addLast(String.valueOf(inputDataCharter));
-        }
+            String checkCharacter = String.valueOf(inputDataCharter);
+            if (checkCharacter.equals(DASH)) { // "-" 포함할 시
+                if(inputPassword.size()> ZERO){
+                    inputPassword.removeLast(); // 데큐에 데이터가 있으면 - 앞 문자 삭제
+                }
+            } else if (checkCharacter.equals(LEFT_INEQUALITY_SIGN)) {
+                if (inputPassword.size() > ZERO) {
+                    leftChangeLocationFlag = false;
+                }
+            } else if (checkCharacter.equals(RIGHT_INEQUALITY_SIGN)) {
 
-//        <<BP<A>>Cd-
-        // List는 반복문에서 요소를 삭제하면 전체 리스트 길이가 변하므로 사용하기 애매.
-        // > 왼쪽, 오른쪽 요소 교환 및 화살표 삭제
-        // < 오른쪽, 왼쪽 요소 교환 및 화살표 삭제
-        // - 하나 앞의 요소 삭제
-        System.out.println(inputPassword.toString());
-        final int passwordLength = inputPassword.size();
-        for (int i = 0; i < passwordLength; i++) {
-            String passwordCharacter = inputPassword.pop();
-            System.out.println("추출: "+passwordCharacter);
-            System.out.println("추출2: "+inputPassword.pollLast());
-
-            if(passwordCharacter.equals(DASH)){
-                System.out.println(inputPassword.removeLast());
-            }else if(passwordCharacter.equals(LEFT_INEQUALITY_SIGN)){
-                System.out.println("in left in");
-                System.out.println(inputPassword.pop());
-            }else if(passwordCharacter.equals(RIGHT_INEQUALITY_SIGN)){
-
-            }else{
-                inputPassword.addLast(passwordCharacter);
+            } else {
+                if(!leftChangeLocationFlag){
+                    String frontCharacter = inputPassword.pop();
+                    inputPassword.push(checkCharacter);
+                    inputPassword.push(frontCharacter);
+                    leftChangeLocationFlag = true; // 왼쪽화살표가 있을 때 앞문자와 순서 바꿈
+                }else{
+                    inputPassword.addLast(checkCharacter); // 입력문자가 화살표와 "-"가 아닌 문자일경우 데큐에 문자 추가.
+                }
             }
-            System.out.println(inputPassword.toString());
         }
-        System.out.println(inputPassword.toString());
-        return null;
+
+        for (String password:inputPassword) {
+            stringBuilder.append(password);
+        }
+// <<BP<A>>Cd-
+
+        return stringBuilder.toString();
     }
 }
