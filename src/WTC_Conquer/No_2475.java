2692262ba913ec2fb95740009f625664987a5037
minsulee <alkwen0996@naver.com>
2021-10-17 17:58:06 +0900

    add WTC_Conquer Commit (Solve No_2475)

---
+++ src/WTC_Conquer/No_2475.java
@@ -0,0 +1,33 @@
+package WTC_Conquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class No_2475 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        final List<Integer> numbers = new ArrayList<>();
+
+        while (stringTokenizer.hasMoreTokens()) {
+            numbers.add(Integer.parseInt(stringTokenizer.nextToken()));
+        }
+
+        System.out.println(solve(numbers));
+
+    }
+
+    private static int solve(final List<Integer> numbers) {
+
+        int verificationNumber = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            verificationNumber = numbers.get(i) * numbers.get(i) + verificationNumber;
+        }
+
+        return verificationNumber % 10;
+    }
+}
