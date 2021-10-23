ed93003630e15920c5cccb70d0d306b22e52a909
minsulee <alkwen0996@naver.com>
2021-10-23 11:21:52 +0900

    add String Commit (Solve No_10953)

---
+++ src/String/No_10953.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_10953 {
+    public static final String DELIMITER = ",";
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        int sum = 0;
+        while(T-->0){
+            final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), DELIMITER);
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            sum = (a+b);
+            stringBuilder.append(sum).append("\n");
+        }
+
+        System.out.println(stringBuilder);
+
+    }
+}
