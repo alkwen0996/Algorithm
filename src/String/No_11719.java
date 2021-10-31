41e6d250dde849e1f27386248201b7f35b90165e
minsulee <alkwen0996@naver.com>
2021-10-31 22:49:58 +0900

    add WTC_Conquer Commit (Solve No_11719)

---
+++ src/String/No_11719.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class No_11719 {
+    public static void main(String[] args) throws IOException {
+        final Scanner scanner = new Scanner(System.in);
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        while(scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
+//    Hello
+//
+//            Baekjoon
+//    Online Judge
