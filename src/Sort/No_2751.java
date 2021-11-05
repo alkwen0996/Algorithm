b65de1acfe280a90ab6854e792f46c3fa084d89d
minsulee <alkwen0996@naver.com>
2021-11-05 15:12:23 +0900

    add Sort Commit (Refactoring No_2751)

--- src/Sort/No_2751.java
+++ src/Sort/No_2751.java
@@ -3,31 +3,33 @@ package Sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class No_2751 {
     public static final String NEW_LINE = "\n";
+    public static final int ARRAY_SIZE = 2_000_001;
+    public static final int ABSOLUTE_VALUE = 1_000_000;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        final List<Integer> numbers = new ArrayList<>();
         int N = Integer.parseInt(bufferedReader.readLine());
+        final boolean[] numbers = new boolean[ARRAY_SIZE];
 
         while (N-- > 0) {
-            numbers.add(Integer.parseInt(bufferedReader.readLine()));
+            int number = Integer.parseInt(bufferedReader.readLine());
+            numbers[number + ABSOLUTE_VALUE] = true;
         }
 
         String result = solve(numbers);
         System.out.println(result);
     }
 
-    private static String solve(final List<Integer> numbers) {
+    private static String solve(final boolean[] numbers) {
         final StringBuilder stringBuilder = new StringBuilder();
-        Collections.sort(numbers);
-        for (Integer number : numbers) {
-            stringBuilder.append(number).append(NEW_LINE);
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i]) {
+                stringBuilder.append(i - ABSOLUTE_VALUE).append(NEW_LINE);
+            }
         }
 
         return stringBuilder.toString();
