8bb86093af652b022cfb95ec435d685b1d382591
minsulee <alkwen0996@naver.com>
2021-11-05 14:59:14 +0900

    add Greedy Commit (Solve No_11047)

---
+++ src/Greedy/No_11047.java
@@ -0,0 +1,41 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class No_11047 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        final int N = Integer.parseInt(stringTokenizer.nextToken());
+        final int K = Integer.parseInt(stringTokenizer.nextToken());
+        final List<Integer> coins = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            coins.add(Integer.parseInt(bufferedReader.readLine()));
+        }
+
+        int result = solve(coins, K);
+        System.out.println(result);
+    }
+
+    private static int solve(final List<Integer> coins, final int k) {
+        int minimumCoinCount = 0;
+
+        int money = k;
+        int share = 1;
+        for (int i = coins.size() - 1; i >= 0; i--) {
+            share = money / coins.get(i);
+            if (share > 0) {
+                minimumCoinCount += share;
+                money = k % coins.get(i);
+            }
+        }
+
+        return minimumCoinCount;
+    }
+}
