69998e51765a48efeb476a0970a68d6cc729db95
minsulee <alkwen0996@naver.com>
2021-11-08 08:44:15 +0900

    add Implementation Commit (Solve No_11055)

---
+++ src/Implementation/No_11055.java
@@ -0,0 +1,34 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class No_11055 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int sequenceSize = scanner.nextInt();
+
+        int[] sequenceArray = new int[sequenceSize];
+
+        for (int i = 0; i < sequenceSize; i++) {
+            sequenceArray[i] = scanner.nextInt();
+        }
+
+        int max = 0;
+        int[] increaseArray = new int[sequenceSize];
+        for (int i = 0; i < sequenceSize; i++) {
+            increaseArray[i] = 0;
+            for (int j = 0; j < i; j++) {
+                if (sequenceArray[i] > sequenceArray[j] && increaseArray[j] + 1 > increaseArray[i]) {
+                    increaseArray[i] = increaseArray[j] + 1;
+                }
+            }
+            if(max<increaseArray[i]){
+                max = increaseArray[i];
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
