25479044eaccd05548539c678ecb2edc0ca8969a
minsulee <alkwen0996@naver.com>
2021-11-23 15:07:23 +0900

    add BFS Commit (Refactoring No_1260)

--- src/Breadth_First_Search/No_1260.java
+++ src/Breadth_First_Search/No_1260.java
@@ -11,91 +11,79 @@ import java.util.StringTokenizer;
 public class No_1260 {
     public static final String NEW_LINE = "\n";
     public static final String SPACE = " ";
-    private static int[][] peeksDFS;
-    private static int[][] peeksBFS;
-    private static boolean[] checkVisitedDFS;
-    private static boolean[] checkVisitedBFS;
-    private static final StringBuilder stringBuilder = new StringBuilder();
+    private static int[][] peeks;
+    private static boolean[] checkVisited;
+    private static final StringBuilder stringBuilderDFS = new StringBuilder();
+    private static final StringBuilder stringBuilderBFS = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        final StringTokenizer stringTokenizer1 = new StringTokenizer(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        final int peeksCount = Integer.parseInt(stringTokenizer1.nextToken());
-        final int linkCount = Integer.parseInt(stringTokenizer1.nextToken());
-        final int startPeek = Integer.parseInt(stringTokenizer1.nextToken());
+        final int peeksCount = Integer.parseInt(stringTokenizer.nextToken());
+        final int linkCount = Integer.parseInt(stringTokenizer.nextToken());
+        final int startPeek = Integer.parseInt(stringTokenizer.nextToken());
 
-        peeksDFS = new int[peeksCount + 1][peeksCount + 1];
-        checkVisitedDFS = new boolean[peeksCount + 1];
-        checkVisitedDFS[startPeek] = true;
-
-        peeksBFS = new int[peeksCount + 1][peeksCount + 1];
-        checkVisitedBFS = new boolean[peeksCount + 1];
-        checkVisitedBFS[startPeek] = true;
+        peeks = new int[peeksCount + 1][peeksCount + 1];
 
         for (int i = 0; i < linkCount; i++) {
-            final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
             int peek1 = Integer.parseInt(stringTokenizer.nextToken());
             int peek2 = Integer.parseInt(stringTokenizer.nextToken());
 
-            peeksDFS[peek1][peek2] = peeksDFS[peek2][peek1] = 1;
-            peeksBFS[peek1][peek2] = peeksBFS[peek2][peek1] = 1;
+            peeks[peek1][peek2] = peeks[peek2][peek1] = 1;
         }
 
-        String result = solution(startPeek);
-        System.out.println(result);
+        checkVisited = new boolean[peeksCount + 1];
+        dfs(startPeek);
+
+        checkVisited = new boolean[peeksCount + 1];
+        bfs(startPeek);
+
+        System.out.println(stringBuilderDFS);
+        System.out.println(stringBuilderBFS);
     }
 
-    private static String solution(final int startPeek) {
-        dfs(startPeek, stringBuilder);
-        stringBuilder.append(NEW_LINE).append(bfs(startPeek));
-
-        return stringBuilder.toString();
-    }
-
-    private static void dfs(final int startPeek, StringBuilder stringBuilder) {
+    private static void dfs(final int startPeek) {
         final Stack<Integer> visitPeeks = new Stack<>();
 
         visitPeeks.push(startPeek);
-        checkVisitedDFS[startPeek] = true;
+        checkVisited[startPeek] = true;
 
-        stringBuilder.append(startPeek).append(SPACE);
+        stringBuilderDFS.append(startPeek).append(SPACE);
 
         while (!visitPeeks.isEmpty()) {
-            for (int i = 1; i < peeksDFS.length; i++) {
-                if (peeksDFS[startPeek][i] == 1 && !checkVisitedDFS[i]) {
+            for (int i = 1; i < peeks.length; i++) {
+                if (peeks[startPeek][i] == 1 && !checkVisited[i]) {
                     visitPeeks.push(i);
-                    checkVisitedDFS[i] = true;
-                    dfs(i, stringBuilder);
+                    checkVisited[i] = true;
+                    dfs(i);
                 }
             }
             visitPeeks.pop();
         }
     }
 
-    private static String bfs(final int startPeek) {
-        final StringBuilder bfsStringBuilder = new StringBuilder();
+    private static void bfs(final int startPeek) {
         final Queue<Integer> visitPeeks = new LinkedList<>();
 
         visitPeeks.add(startPeek);
-        bfsStringBuilder.append(startPeek).append(SPACE);
-        checkVisitedBFS[startPeek] = true;
+        checkVisited[startPeek] = true;
+        stringBuilderBFS.append(startPeek).append(SPACE);
 
         while (!visitPeeks.isEmpty()) {
-
             int visitedPeek = visitPeeks.poll();
-            for (int i = 1; i < peeksBFS.length; i++) {
 
-                if (peeksBFS[visitedPeek][i] == 1 && !checkVisitedBFS[i]) {
-                    checkVisitedBFS[i] = true;
+            for (int i = 1; i < peeks.length; i++) {
+                if (peeks[visitedPeek][i] == 1 && !checkVisited[i]) {
+                    checkVisited[i] = true;
                     visitPeeks.add(i);
-                    bfsStringBuilder.append(i).append(SPACE);
-                }
 
+                    stringBuilderBFS.append(i).append(SPACE);
+                }
             }
         }
 
-        return bfsStringBuilder.append(NEW_LINE).toString();
     }
 }
