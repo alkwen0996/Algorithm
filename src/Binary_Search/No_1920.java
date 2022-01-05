2a8696db2739528df1dd2d21e118242719ee665f
minsulee <alkwen0996@naver.com>
2022-01-05 14:39:20 +0900

    add Binary_Search Commit (second solve No_1920)

--- src/Binary_Search/No_1920.java
+++ src/Binary_Search/No_1920.java
@@ -3,7 +3,8 @@ package Binary_Search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class No_1920 {
@@ -16,59 +17,26 @@ public class No_1920 {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         final int N = Integer.parseInt(bufferedReader.readLine());
-        final int[] haveNumbers = new int[N];
+        Map<Integer, Boolean> compareNumbers = new LinkedHashMap<>();
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         for (int i = 0; i < N; i++) {
-            haveNumbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+            compareNumbers.put(Integer.parseInt(stringTokenizer.nextToken()), true);
         }
 
         final int M = Integer.parseInt(bufferedReader.readLine());
-        final int[] compareNumbers = new int[M];
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        for (int i = 0; i < M; i++) {
-            compareNumbers[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-
-        String result = solve(haveNumbers, compareNumbers);
-        System.out.println(result);
-    }
-
-    private static String solve(final int[] haveNumbers, final int[] compareNumbers) {
         final StringBuilder stringBuilder = new StringBuilder();
 
-        Arrays.sort(haveNumbers);
-
-        for (final int compareNumber : compareNumbers) {
-            if (binarySearch(compareNumber, haveNumbers)) {
+        for (int i = 0; i < M; i++) {
+            if (compareNumbers.containsKey(Integer.parseInt(stringTokenizer.nextToken()))) {
                 stringBuilder.append(CONTAINS).append(NEW_LINE);
-                continue;
-            }
-            stringBuilder.append(NOT_CONTAINS).append(NEW_LINE);
-        }
-
-        return stringBuilder.toString();
-    }
-
-    private static boolean binarySearch(final int compareNumber, final int[] haveNumbers) {
-        int leftIndex = 0;
-        int rightIndex = haveNumbers.length - 1;
-
-        while (leftIndex <= rightIndex) {
-            int midIndex = (leftIndex + rightIndex) / 2;
-            int midValue = haveNumbers[midIndex];
-
-            if (midValue < compareNumber) {
-                leftIndex = midIndex + 1;
-            } else if (midValue > compareNumber) {
-                rightIndex = midIndex - 1;
             } else {
-                return true;
+                stringBuilder.append(NOT_CONTAINS).append(NEW_LINE);
             }
-
         }
 
-        return false;
+        System.out.println(stringBuilder);
     }
 }
