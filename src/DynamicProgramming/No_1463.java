e49a1f57f219c8dbb56652712d3f979519c65f0a
minsulee <alkwen0996@naver.com>
2022-01-06 14:18:45 +0900

    add Dynamic_Programming Commit (solve No_1463)

--- src/DynamicProgramming/No_1463.java
+++ src/DynamicProgramming/No_1463.java
@@ -5,18 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No_1463 {
+    private static int[] dp;
+
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
+        int x = Integer.parseInt(bufferedReader.readLine());
 
-        int result = solve(N);
+        dp = new int[x + 1];
+        dp[0] = dp[1] = 0;
+
+        int result = recursive(x);
         System.out.println(result);
     }
 
-    private static int solve(final int n) {
+    private static int recursive(int x) {
+        for (int i = 2; i <= x; i++) {
+            dp[i] = dp[i - 1] + 1;
 
-        
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
 
-        return 0;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+
+        }
+
+        return dp[x];
     }
 }
