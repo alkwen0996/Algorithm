d72954e243044d83c49b5310aa71aa5477d70f79
minsulee <alkwen0996@naver.com>
2022-01-07 13:51:09 +0900

    add BFS Commit (solve No_2178)

--- src/BFS_DFS/No_2178.java
+++ src/BFS_DFS/No_2178.java
@@ -8,56 +8,67 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class No_2178 {
-    private static int[][] maze;
-    private static boolean[] checkVisited;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int[] widthSearch = {1, 0, -1, 0};
+    private static int[] heightSearch = {0, 1, 0, -1};
+    private static Queue<Node> route;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        final int rowSize = Integer.parseInt(stringTokenizer.nextToken());
-        final int columnSize = Integer.parseInt(stringTokenizer.nextToken());
+        final int N = Integer.parseInt(stringTokenizer.nextToken());
+        final int M = Integer.parseInt(stringTokenizer.nextToken());
 
-        maze = new int[rowSize + 1][columnSize + 1];
-        checkVisited = new boolean[101];
+        map = new int[N][M];
+        visited = new boolean[N][M];
 
-        for (int i = 0; i < rowSize; i++) {
-            String inputLine = bufferedReader.readLine();
-            for (int j = 0; j < columnSize; j++) {
-                maze[i][j] = Integer.parseInt(String.valueOf(inputLine.charAt(j)));
+        for (int i = 0; i < N; i++) {
+            char[] mapLine = bufferedReader.readLine().toCharArray();
+
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(String.valueOf(mapLine[j]));
             }
         }
 
-        int result = solution();
-        System.out.println(result);
+        int shortestRoute = bfs(N, M);
+        System.out.println(shortestRoute);
     }
 
-    private static int solution() {
-        int startPoint = 1;
+    private static int bfs(final int N, final int M) {
+        visited[0][0] = true;
+        route = new LinkedList<>();
+        route.add(new Node(0, 0));
 
-        return bfs(startPoint);
-    }
+        while (!route.isEmpty()) {
+            Node node = route.poll();
 
-    private static int bfs(final int startPoint) {
-        int visitedAreaCount = 0;
-        final Queue<Integer> areas = new LinkedList<>();
+            for (int i = 0; i < widthSearch.length; i++) {
+                int positionX = node.x + widthSearch[i];
+                int positionY = node.y + heightSearch[i];
 
-        areas.add(startPoint);
-        checkVisited[1] = true;
-
-        while (!areas.isEmpty()) {
-            int visitedArea = areas.poll();
-            for (int i = 1; i < maze.length; i++) {
-                if (maze[visitedArea][i] == 1 && !checkVisited[i]) {
-                    areas.add(i);
-                    checkVisited[i] = true;
-                    visitedAreaCount++;
+                if (positionX >= 0 && positionY >= 0 && positionX < N && positionY < M) {
+                    if (map[positionX][positionY] == 1 && !visited[positionX][positionY]) {
+                        visited[positionX][positionY] = true;
+                        map[positionX][positionY] = map[node.x][node.y] + 1;
+                        route.add(new Node(positionX, positionY));
+                    }
                 }
             }
         }
 
-        System.out.println("visitedAreaCount: " + visitedAreaCount);
-
-        return visitedAreaCount;
+        return map[N-1][M-1];
     }
+
+    static class Node {
+        private int x;
+        private int y;
+
+        public Node(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
 }
