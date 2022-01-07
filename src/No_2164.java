afac66ec3deae2e0be38aa8a93a6899a07dc83d4
minsulee <alkwen0996@naver.com>
2022-01-07 14:14:20 +0900

    add Data_Structure Commit (solve No_2178)

---
+++ src/No_2164.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class No_2164 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(bufferedReader.readLine());
+
+        Deque<Integer> numbers = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            numbers.add(i);
+        }
+
+        int result = solve(numbers);
+        System.out.println(result);
+    }
+
+    private static int solve(final Deque<Integer> numbers) {
+        int lastCardNumber = 0;
+
+        while (numbers.size() > 1) {
+            numbers.pop();
+            numbers.add(numbers.pop());
+        }
+
+        lastCardNumber = numbers.pop();
+
+        return lastCardNumber;
+    }
+}
