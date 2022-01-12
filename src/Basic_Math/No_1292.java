a5fd0d47949c69c33428805978a3ad9528a0f821
minsulee <alkwen0996@naver.com>
2022-01-12 11:42:57 +0900

    add Math Commit (Solve No_1292)

---
+++ src/Basic_Math/No_1292.java
@@ -0,0 +1,47 @@
+package Basic_Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_1292 {
+    private static int[] progression;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        final int a = Integer.parseInt(stringTokenizer.nextToken());
+        final int b = Integer.parseInt(stringTokenizer.nextToken());
+        progression = new int[1001];
+
+        int result = solve(a, b, progression);
+        System.out.println(result);
+    }
+
+    private static int solve(final int a, final int b, final int[] progression) {
+        int number = 1;
+        int count = 1;
+
+        for (int i = 0; i < progression.length; i++) {
+            progression[i] = number;
+
+            if (number == count) {
+                number++;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < progression.length; i++) {
+            if (i + 1 >= a && i + 1 <= b) {
+                sum += progression[i];
+            }
+        }
+
+        return sum;
+    }
+}
