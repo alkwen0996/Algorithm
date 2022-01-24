20f5a0ddea188e1ee1c7d05608076c9d59f1cb15
minsulee <alkwen0996@naver.com>
2022-01-24 12:48:00 +0900

    commit Math (Solve No_1676)

---
+++ src/Math/No_1676.java
@@ -0,0 +1,26 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_1676 {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int n = Integer.parseInt(bufferedReader.readLine());
+
+        int result = solve(n);
+        System.out.println(result);
+    }
+
+    private static int solve(int n) {
+        int zeroCount = 0;
+
+        while (n >= 5) {
+            zeroCount += n / 5;
+            n /= 5;
+        }
+
+        return zeroCount;
+    }
+}
