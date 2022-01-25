6ec45e6b19360b968e4f522a6dc44c3f7051526d
minsulee <alkwen0996@naver.com>
2022-01-25 14:09:58 +0900

    commit Math (Solve No_17103)

---
+++ src/Math/No_17103.java
@@ -0,0 +1,42 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_17103 {
+    static final int arrSize = 1000000;
+    private static final String NEW_LINE = "\n";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] primeNumbers = new boolean[arrSize + 1];
+
+        primeNumbers[0] = primeNumbers[1] = true;
+
+        for (int i = 2; i * i <= arrSize; i++) {
+            if (!primeNumbers[i]) {
+                for (int j = i + i; j <= arrSize; j += i) {
+                    primeNumbers[j] = true;
+                }
+            }
+        }
+
+        int lineCnt = Integer.parseInt(bufferedReader.readLine());
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < lineCnt; i++) {
+            int count = 0;
+            int num = Integer.parseInt(bufferedReader.readLine());
+
+            for (int j = 2; j <= num / 2; j++) {
+                if (!primeNumbers[j] && !primeNumbers[num - j])
+                    count++;
+            }
+
+            stringBuilder.append(count).append(NEW_LINE);
+        }
+
+        System.out.println(stringBuilder);
+    }//main
+}
