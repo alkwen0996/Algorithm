71e75b91d53f692c3ddb159c5e13cfb68bbef3f5
minsulee <alkwen0996@naver.com>
2022-02-07 17:09:27 +0900

    commit Queue (Solve No_42586)

---
+++ src/Programmers/Queue/No_42586.java
@@ -0,0 +1,56 @@
+package Programmers.Queue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class No_42586 {
+    public static void main(String[] args) {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        int[] result = solve(progresses, speeds);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] solve(final int[] progresses, final int[] speeds) {
+        int[] completeFunction = new int[progresses.length];
+        int answerIndex = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+            int completeCount = 0;
+
+            while (progresses[i] < 100) {
+                progresses[i] += speeds[i];
+                completeCount++;
+            }
+
+            if (progresses[i] >= 100) {
+                completeFunction[answerIndex] = completeCount;
+                answerIndex++;
+            }
+        }
+
+        final List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < completeFunction.length; i++) {
+            int distributionCount = 1;
+            if (completeFunction[i] < 0) {
+                continue;
+            }
+
+            for (int j = i + 1; j < completeFunction.length; j++) {
+                if (completeFunction[i] >= completeFunction[j]) {
+                    completeFunction[j] = -1;
+                    distributionCount++;
+                } else {
+                    break;
+                }
+            }
+
+            answer.add(distributionCount);
+        }
+
+        return answer.stream().mapToInt(x -> x).toArray();
+    }
+}
