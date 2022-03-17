53911e6df79d75d22a3ddf873ef5b013cca1f205
minsulee <alkwen0996@naver.com>
2022-03-17 16:39:30 +0900

    commit reSolve BOJ_1476 (Brute_Force)

--- src/BaekJoon/Brute_Force/BOJ_1476.java
+++ src/BaekJoon/Brute_Force/BOJ_1476.java
@@ -3,68 +3,47 @@ package BaekJoon.Brute_Force;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_1476 {
-    private static final int YEAR_SIZE = 3;
-    private static final int EARTH_RANGE = 15;
-    private static final int SUN_RANGE = 28;
-    private static final int MOON_RANGE = 19;
-
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        final int[] year = new int[YEAR_SIZE];
+        final int e = Integer.parseInt(stringTokenizer.nextToken());
+        final int s = Integer.parseInt(stringTokenizer.nextToken());
+        final int m = Integer.parseInt(stringTokenizer.nextToken());
 
-        for (int i = 0; i < year.length; i++) {
-            year[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-
-        int result = solution(year);
+        int result = solution(e, s, m);
         System.out.println(result);
     }
 
-    private static int solution(final int[] year) {
-        int yearCount = 1;
-        int[] compareYear = new int[year.length];
+    private static int solution(final int e, final int s, final int m) {
+        int year = 1;
 
-        Arrays.fill(compareYear, 1);
+        int countE = 1;
+        int countS = 1;
+        int countM = 1;
 
-        while (isSameYear(year, compareYear)) {
-            for (int i = 0; i < compareYear.length; i++) {
-                compareYear[i]++;
+        while (countE != e || countS != s || countM != m) {
+            if (countE == 15) {
+                countE = 0;
             }
 
-            yearCount++;
-        }
-
-        return yearCount;
-    }
-
-    private static boolean isSameYear(final int[] year, final int[] compareYear) {
-        boolean isSameYear = false;
-
-        for (int i = 0; i < year.length; i++) {
-            if (year[i] != compareYear[i]) {
-                isSameYear = true;
-                break;
+            if (countS == 28) {
+                countS = 0;
             }
+
+            if (countM == 19) {
+                countM = 0;
+            }
+
+            countE++;
+            countS++;
+            countM++;
+            year++;
         }
 
-        if (compareYear[0] == EARTH_RANGE) {
-            compareYear[0] = 0;
-        }
-
-        if (compareYear[1] == SUN_RANGE) {
-            compareYear[1] = 0;
-        }
-
-        if (compareYear[2] == MOON_RANGE) {
-            compareYear[2] = 0;
-        }
-
-        return isSameYear;
+        return year;
     }
 }
