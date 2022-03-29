731af7e4a4a7590442cd567879c4aafcaa25f3d0
minsulee <alkwen0996@naver.com>
2022-03-29 17:48:51 +0900

    commit reSolve BOJ_1759 (Brute_Force)

--- src/BaekJoon/Brute_Force/BOJ_1759.java
+++ src/BaekJoon/Brute_Force/BOJ_1759.java
@@ -10,21 +10,14 @@ public class BOJ_1759 {
     private static final StringBuilder stringBuilder = new StringBuilder();
     private static final String NEW_LINE = "\n";
 
-    private static String[] totalLetters;
-    private static boolean[] checkUsingLetters;
-    private static int targetLetterCount;
-    private static String[] useLetters;
-
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
-        targetLetterCount = Integer.parseInt(stringTokenizer.nextToken());
-        useLetters = new String[targetLetterCount];
+        int targetLetterCount = Integer.parseInt(stringTokenizer.nextToken());
 
         final int totalLetterCount = Integer.parseInt(stringTokenizer.nextToken());
-        totalLetters = new String[totalLetterCount];
-        checkUsingLetters = new boolean[totalLetterCount];
+        String[] totalLetters = new String[totalLetterCount];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
@@ -35,44 +28,35 @@ public class BOJ_1759 {
         Arrays.sort(totalLetters);
 
         int index = 0;
-        int start = 0;
+        String password = "";
 
-        findPassword(index, start);
+        findPassword(targetLetterCount, totalLetters, password, index);
         System.out.println(stringBuilder);
     }
 
-    private static void findPassword(final int index, int start) {
-        if (index == targetLetterCount) {
-            if (checkContainsVowels()) {
-                for (final String useLetter : useLetters) {
-                    stringBuilder.append(useLetter);
-                }
-
-                stringBuilder.append(NEW_LINE);
+    private static void findPassword(final int targetLetterCount, final String[] totalLetters, final String password, final int index) {
+        if (password.length() == targetLetterCount) {
+            if (checkContainsVowels(password)) {
+                stringBuilder.append(password).append(NEW_LINE);
             }
 
             return;
         }
 
-        for (int i = start; i < totalLetters.length; i++) {
-            if (checkUsingLetters[i]) {
-                continue;
-            }
-
-            checkUsingLetters[i] = true;
-            useLetters[index] = totalLetters[i];
-            start = i;
-            findPassword(index + 1, start);
-            checkUsingLetters[i] = false;
+        if (index >= totalLetters.length) {
+            return;
         }
+
+        findPassword(targetLetterCount, totalLetters, password + totalLetters[index], index + 1);
+        findPassword(targetLetterCount, totalLetters, password, index + 1);
     }
 
-    private static boolean checkContainsVowels() {
+    private static boolean checkContainsVowels(final String password) {
         int vowelCount = 0;
         int consonantCount = 0;
 
-        for (final String useLetter : useLetters) {
-            if (useLetter.equals("a") || useLetter.equals("e") || useLetter.equals("i") || useLetter.equals("o") || useLetter.equals("u")) {
+        for (final char useLetter : password.toCharArray()) {
+            if (useLetter == 'a' || useLetter == 'e' || useLetter == 'i' || useLetter == 'o' || useLetter == 'u') {
                 vowelCount += 1;
                 continue;
             }
