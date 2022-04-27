93af1a0c3d5aa567f518661735fbf60bab2d0ce1
minsulee <alkwen0996@naver.com>
2022-04-27 22:23:44 +0900

    commit resolve BOJ_1463 (DynamicProgramming)

---
+++ src/BaekJoon/DynamicProgramming/BOJ_1463.java
@@ -0,0 +1,42 @@
+package BaekJoon.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1463 {
+    private static int result = 100_000;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        int count = 0;
+        createOne(n, count);
+
+        System.out.println(result);
+    }
+
+    private static void createOne(int n, int count) {
+        if (n == 1) {
+            result = Math.min(count, result);
+
+            return;
+        }
+
+        if (count >= result) {
+            return;
+        }
+
+        if (n % 3 == 0) {
+            createOne(n / 3, count + 1);
+        }
+
+        if (n % 2 == 0) {
+            createOne(n / 2, count + 1);
+        }
+
+        createOne(n - 1, count + 1);
+    }
+
+}
