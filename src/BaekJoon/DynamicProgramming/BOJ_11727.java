2e291bc0ea222e9363e74aa5918777b77c962687
minsulee <alkwen0996@naver.com>
2022-06-20 22:47:00 +0900

    commit solve BOJ_11727

--- src/BaekJoon/DynamicProgramming/BOJ_11727.java
+++ src/BaekJoon/DynamicProgramming/BOJ_11727.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_11727 {
+    private static final int MAXIMUM_RANGE = 10_007;
     private static int[] memorization;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         final int n = Integer.parseInt(bufferedReader.readLine());
 
-        memorization = new int[10_007];
+        memorization = new int[MAXIMUM_RANGE];
         memorization[1] = 1;
         memorization[2] = 3;
 
@@ -24,7 +25,7 @@ public class BOJ_11727 {
         }
 
         for (int i = 3; i <= n; i++) {
-            memorization[i] = (memorization[i - 1] + (2 * memorization[i - 2])) % 10_007;
+            memorization[i] = (memorization[i - 1] + (2 * memorization[i - 2])) % MAXIMUM_RANGE;
         }
 
         return memorization[n];
