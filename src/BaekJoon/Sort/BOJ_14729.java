a630e654c0d756fe8e6c6958b698142eff339891
minsulee <alkwen0996@naver.com>
2022-07-07 09:31:12 +0900

    commit solve BOJ_14729

--- src/BaekJoon/Sort/BOJ_14729.java
+++ src/BaekJoon/Sort/BOJ_14729.java
@@ -3,7 +3,9 @@ package BaekJoon.Sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class BOJ_14729 {
 
@@ -14,25 +16,24 @@ public class BOJ_14729 {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         final int countOfStudents = Integer.parseInt(bufferedReader.readLine());
 
-        final float[] scores = new float[countOfStudents];
+        final float[] scores = new float[10_000_001];
 
         for (int i = 0; i < countOfStudents; i++) {
-            scores[i] = Float.parseFloat(bufferedReader.readLine());
+            scores[(int) (Float.parseFloat(bufferedReader.readLine()) * 1000)]++;
         }
-
-        System.out.println(solve(scores));
-    }
-
-    private static StringBuilder solve(final float[] scores) {
-        Arrays.sort(scores);
 
         final StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
 
-        for (int i = 0; i < 7; i++) {
-            stringBuilder.append(String.format(FORMAT, scores[i])).append(NEW_LINE);
+        for (int i = 0; i < scores.length; i++) {
+            while (scores[i] > 0 && count < 7) {
+                stringBuilder.append(String.format(FORMAT, i / 1000.0)).append(NEW_LINE);
+                count++;
+                scores[i]--;
+            }
         }
 
-        return stringBuilder;
+        System.out.println(stringBuilder);
     }
 
 }
