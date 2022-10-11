850e951dde6aeb782696331fb5fe57fe97037b81
Jerome <34956859+alkwen0996@users.noreply.github.com>
2022-10-11 14:04:19 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,713 ms, Memory: 105,076 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-    private static int count;
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -22,7 +20,6 @@ public class Solution {
             final int compareCount = Integer.parseInt(bufferedReader.readLine());
 
             final int[][] relations = new int[studentCount + 1][studentCount + 1];
-            final int[][] reverseRelations = new int[studentCount + 1][studentCount + 1];
             StringTokenizer stringTokenizer;
 
             for (int j = 1; j <= compareCount; j++) {
@@ -32,21 +29,35 @@ public class Solution {
                 final int bigTall = Integer.parseInt(stringTokenizer.nextToken());
 
                 relations[smallTall][bigTall] = 1;
-                reverseRelations[bigTall][smallTall] = 1;
             }
 
             int result = 0;
 
+            for (int j = 1; j <= studentCount; j++) { //경유 500
+                for (int k = 1; k <= studentCount; k++) { // 출발 500
+                    if (j == k) {
+                        continue;
+                    }
+
+                    for (int l = 1; l <= studentCount; l++) { // 도착 500
+                        if (relations[k][l] == 1) {
+                            continue;
+                        }
+
+                        relations[k][l] = relations[k][j] & relations[j][l];
+                    }
+                }
+            }
+
             for (int j = 1; j <= studentCount; j++) {
-                count = 0;
-                bfs(relations, j);
-                bfs(reverseRelations, j);
-/*dfs 코드
-                count = 0;
-                dfs(relations, j, new boolean[relations.length]);
-                dfs(reverseRelations, j, new boolean[relations.length]);
-*/
-                if (count == studentCount - 1) {
+                for (int k = 1; k <= studentCount; k++) {
+                    relations[j][0] += relations[j][k]; // i보다 큰 j가 카운트에 누적
+                    relations[0][k] += relations[j][k]; // j보다 작은 i가 카운트에 누적
+                }
+            } // 0행 0열에 큰, 작은 학생수를 각각 저장.
+
+            for (int j = 1; j <= studentCount; j++) {
+                if (relations[0][j] + relations[j][0] == studentCount - 1) {
                     result++;
                 }
             }
@@ -55,42 +66,6 @@ public class Solution {
         }
 
         System.out.println(stringBuilder);
-    }
-/*
-    private static void dfs(final int[][] relations, final int vertex, final boolean[] visited) {
-        visited[vertex] = true;
-
-        for (int i = 1; i < relations.length; i++) {
-            if (relations[i][vertex] == 0 || visited[i]) {
-                continue;
-            }
-
-            count++;
-            dfs(relations, i, visited);
-        }
-    }
-*/
-
-    private static void bfs(final int[][] relations, final int startVertex) {
-        final Deque<Integer> queue = new ArrayDeque<>();
-        final boolean[] visited = new boolean[relations.length];
-        visited[startVertex] = true;
-        queue.add(startVertex);
-
-        while (!queue.isEmpty()) {
-            final Integer currentVertex = queue.poll();
-
-            for (int i = 1; i < relations[currentVertex].length; i++) {
-                if (visited[i] || relations[i][currentVertex] == 0) {
-                    continue;
-                }
-
-                queue.add(i);
-                visited[i] = true;
-                count++;
-            }
-        }
-
     }
 
 }
