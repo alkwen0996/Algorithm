a3bea28ab0cf3f9a4132478cd539d9e1644e901a
Jerome <34956859+alkwen0996@users.noreply.github.com>
2022-10-16 16:47:48 +0900

    [Silver III] Title: N과 M (1), Time: 292 ms, Memory: 22348 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static boolean[] useNumber = new boolean[10];
+    static int[] sequence = new int[10];
+
+    private static final String SPACE = " ";
+    private static final String NEW_LINE = "\n";
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        final int n = Integer.parseInt(stringTokenizer.nextToken());
+        final int m = Integer.parseInt(stringTokenizer.nextToken());
+
+        int index = 0;
+
+        findSequence(index, n, m);
+        System.out.println(stringBuilder.toString());
+    }
+
+    private static void findSequence(int index, final int n, final int m) {
+        if (index == m) {
+            for (int i = 0; i < m; i++) {
+                stringBuilder.append(sequence[i]);
+
+                if (i < m - 1) {
+                    stringBuilder.append(SPACE);
+                }
+            }
+
+            stringBuilder.append(NEW_LINE);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (useNumber[i]) {
+                continue;
+            }
+
+            useNumber[i] = true;
+            sequence[index] = i;
+            findSequence(index + 1, n, m);
+            useNumber[i] = false;
+        }
+    }
+}
