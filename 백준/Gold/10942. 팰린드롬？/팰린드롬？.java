86411aa2acce8dac12c210ed9abf1cc8df120c6b
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-07-15 17:04:47 +0900

    [Gold IV] Title: 팰린드롬?, Time: 888 ms, Memory: 256948 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,15 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static final String NEW_LINE = "\n";
+    public static final int OK = 1;
+    public static final int NO = 0;
     private static int[] numbers;
+    private static int n;
+    private static boolean[][] isPalindrome;
+    private static List<Range> ranges;
 
     public static void main(String[] args) throws IOException {
+        inputData();
+        checkPalindrome();
+        print();
+    }
+
+    private static void print() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Range range : ranges) {
+            if (isPalindrome[range.startIndex][range.endIndex]) {
+                stringBuilder.append(OK).append(NEW_LINE);
+                continue;
+            }
+
+            stringBuilder.append(NO).append("\n");
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    private static void checkPalindrome() {
+        for (int i = 1; i <= n; i++) {
+            isPalindrome[i][i] = true; // 1개 팰린드롬
+        }
+
+        for (int i = 1; i <= n - 1; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                isPalindrome[i][i + 1] = true; // 2개 팰린드롬
+            }
+        }
+
+        for (int i = 3; i <= n; i++) { // 팰린드롬 길이
+            for (int j = 1; j <= n - i + 1; j++) { // 시작점
+                int end = j + i - 1; // 끝점
+
+                if (numbers[j] == numbers[end] && isPalindrome[j + 1][end - 1]) {
+                    isPalindrome[j][end] = true;
+                }
+            }
+        }
+    }
+
+    private static void inputData() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
+        n = Integer.parseInt(bufferedReader.readLine());
         numbers = new int[n + 1];
+        isPalindrome = new boolean[n + 1][n + 1];
 
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
@@ -18,31 +71,25 @@ public class Main {
         }
 
         int m = Integer.parseInt(bufferedReader.readLine());
-        int start, end;
-
-        StringBuilder stringBuilder = new StringBuilder();
+        ranges = new ArrayList<>();
 
         for (int i = 0; i < m; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            start = Integer.parseInt(stringTokenizer.nextToken());
-            end = Integer.parseInt(stringTokenizer.nextToken());
-
-            stringBuilder.append(isPalindrome(start, end)).append("\n");
+            ranges.add(new Range(
+                    Integer.parseInt(stringTokenizer.nextToken()),
+                    Integer.parseInt(stringTokenizer.nextToken()))
+            );
         }
-
-        System.out.println(stringBuilder);
     }
 
-    private static int isPalindrome(final int start, final int end) {
-        int mid = (end - start) / 2;
+    static class Range {
+        private int startIndex;
+        private int endIndex;
 
-        for (int i = 0; i <= mid; i++) {
-            if (numbers[start + i] != numbers[end - i]) {
-                return 0;
-            }
+        public Range(final int startIndex, final int endIndex) {
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
         }
-
-        return 1;
     }
 
 }
