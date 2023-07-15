0d97a0bf38cb763f7e7ce9b1a061db779109ae73
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-07-15 16:15:47 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1280 ms, Memory: 214392 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,33 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
 public class Main {
-	static int[] arr;
-	static int n;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n+1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=1;i<n+1;i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
-		}
-		int t = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<t;i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			sb.append(check(start,end)+"\n");
-		}
-		System.out.println(sb);
-	}
-	static int check(int start,int end) {
-		int mid = (end-start)/2;
-		for(int i=0;i<=mid;i++) {
-			if(arr[start+i]!=arr[end-i]) return 0;
-		}
-		return 1;
-	}
+    private static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        numbers = new int[n + 1];
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int start, end;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            start = Integer.parseInt(stringTokenizer.nextToken());
+            end = Integer.parseInt(stringTokenizer.nextToken());
+
+            stringBuilder.append(isPalindrome(start, end)).append("\n");
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    private static int isPalindrome(final int start, final int end) {
+        int mid = (end - start) / 2;
+
+        for (int i = 0; i <= mid; i++) {
+            if (numbers[start + i] != numbers[end - i]) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
 
 }
