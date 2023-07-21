950699dffc127b042b5367bdc27eb7c8e17f58df
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-07-21 11:44:56 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 860 ms, Memory: 127072 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,81 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static final String NEW_LINE = "\n";
+    private static int n;
+    private static int[][] map;
+    private static List<Range> ranges;
 
     public static void main(String[] args) throws IOException {
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        final int N = Integer.parseInt(stringTokenizer.nextToken());
-        final int M = Integer.parseInt(stringTokenizer.nextToken());
-        final int[][] map = new int[N + 1][N + 1];
-        final int[][] sum = new int[N + 1][N + 1];
+        inputData();
+        getPrefixSum();
+    }
 
-        for (int i = 1; i <= N; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    private static void getPrefixSum() {
+        StringBuilder stringBuilder = new StringBuilder();
 
-            for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+        int[][] prefixSumMap = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefixSumMap[i][j] = prefixSumMap[i - 1][j] + prefixSumMap[i][j - 1] - prefixSumMap[i - 1][j - 1] + map[i][j];
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                sum[i][j] = map[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
-            }
-        }
+        for (Range range : ranges) {
+            Point start = range.start;
+            Point end = range.end;
 
-        final StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < M; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-            int x1 = Integer.parseInt(stringTokenizer.nextToken());
-            int y1 = Integer.parseInt(stringTokenizer.nextToken());
-            int x2 = Integer.parseInt(stringTokenizer.nextToken());
-            int y2 = Integer.parseInt(stringTokenizer.nextToken());
-
-            stringBuilder.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]).append("\n");
+            int result = prefixSumMap[end.x][end.y] - prefixSumMap[end.x][start.y - 1] - prefixSumMap[start.x - 1][end.y] + prefixSumMap[start.x - 1][start.y - 1];
+            stringBuilder.append(result).append("\n");
         }
 
         System.out.println(stringBuilder);
     }
+
+    private static void inputData() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        map = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+        }
+
+        ranges = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            ranges.add(new Range(
+                    new Point(Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken())),
+                    new Point(Integer.parseInt(stringTokenizer.nextToken()), Integer.parseInt(stringTokenizer.nextToken()))
+            ));
+        }
+    }
+
+    static class Range {
+        private final Point start;
+        private final Point end;
+
+        public Range(final Point start, final Point end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
 }
