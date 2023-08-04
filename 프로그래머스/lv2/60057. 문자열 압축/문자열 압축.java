69cfd41d324aedb114a4b9f1e7add4a38e9d722f
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-08-04 09:57:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.15 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -0,0 +1,68 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int solution(String s) {
+        if (s.length() == 1) {
+            return 1;
+        }
+        
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 1; i < s.length(); i++) {
+            answer = Math.min(answer, compressString(i, s));
+        }
+
+        return answer;
+    }
+
+    private int compressString(final int length, final String s) {
+        Deque<String> queue = new ArrayDeque<>();
+
+        int index = 0;
+        String substring;
+
+        while (index <= s.length()) {
+            if (index + length > s.length()) {
+                substring = s.substring(index);
+            } else {
+                substring = s.substring(index, index + length);
+            }
+
+            queue.add(substring);
+            index = index + length;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int sameCount = 1;
+        String prev = queue.poll();
+
+        while (!queue.isEmpty()) {
+            String poll = queue.poll();
+
+            if (prev.equals(poll)) {
+                sameCount++;
+                continue;
+            }
+
+            if (sameCount == 1) {
+                stringBuilder.append(prev);
+            } else {
+                stringBuilder.append(sameCount).append(prev);
+            }
+
+            prev = poll;
+            sameCount = 1;
+        }
+
+        if (prev.length() > 0) {
+            if (sameCount == 1) {
+                stringBuilder.append(prev);
+            } else {
+                stringBuilder.append(sameCount).append(prev);
+            }
+        }
+
+        return stringBuilder.length();
+    }
+}
