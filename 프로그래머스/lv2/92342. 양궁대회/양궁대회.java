070b2519cc33899c99ef12a8c1d20842bded2fb0
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-08-28 14:48:49 +0900

    [level 2] Title: 양궁대회, Time: 18.20 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -2,12 +2,12 @@ class Solution {
     public int maxGap;
     public int[] answer, lionInfo;
     
-        public int[] solution(final int n, final int[] info) {
+    public int[] solution(final int n, final int[] info) {
         maxGap = Integer.MIN_VALUE;
         lionInfo = new int[info.length];
         answer = new int[info.length];
 
-        dfs(info.length - 1, 0, n, info, lionInfo);
+        dfs(0, n, info, lionInfo);
 
         if (maxGap == Integer.MIN_VALUE) {
             return new int[]{-1};
@@ -15,62 +15,69 @@ class Solution {
 
         return answer;
     }
-
-    public void dfs(final int startIndex, final int depth, final int n, final int[] info, final int[] lionInfo) {
-        if (depth == n) {
-            int aPeachScore = 0;
-            int lionScore = 0;
-            int scoreGap;
-
-            for (int i = 0; i < info.length; i++) {
-                if (lionInfo[i] == 0 && info[i] == 0) {
-                    continue;
-                }
-
-                if (info[i] >= lionInfo[i]) {
-                    aPeachScore += (10 - i);
-                }
-
-                if (lionInfo[i] > info[i]) {
-                    lionScore += (10 - i);
-                }
-            }
-
-            scoreGap = lionScore - aPeachScore;
-
-            if (scoreGap <= 0) {
-                return;
-            }
-
-            if (maxGap < scoreGap) {
-                maxGap = scoreGap;
-                answer = lionInfo.clone();
-
-                return;
-            }
+    
+    public void dfs(final int startIndex, final int n, final int[] info, final int[] lionInfo) {
+        if (n == 0) {
+            checkWinner(info, lionInfo);
 
             return;
         }
 
-        for (int i = startIndex; i >= 0; i--) {
+        for (int i = startIndex; i < info.length; i++) {
             lionInfo[i]++;
-            dfs(i, depth + 1, n, info, lionInfo);
+            dfs(i, n - 1, info, lionInfo);
             lionInfo[i]--;
         }
 
     }
 
-    public boolean checkArr(final int[] answer, final int[] lionInfo) {
-        int lionIndex = lionInfo.length;
+    private void checkWinner(final int[] info, final int[] lionInfo) {
+        int aPeachScore = 0;
+        int lionScore = 0;
+        int scoreGap;
 
-        for (int i = lionInfo.length - 1; i >= 0; i--) {
-            if (lionInfo[i] > answer[i]) {
-                lionIndex = i;
-                break;
+        for (int i = 0; i < info.length; i++) {
+            if (info[i] > lionInfo[i]) {
+                aPeachScore += (10 - i);
+            } else {
+                if (lionInfo[i] == 0 && info[i] == 0) {
+                    continue;
+                }
+
+                if (info[i] == lionInfo[i]) {
+                    aPeachScore += (10 - i);
+                } else {
+                    lionScore += (10 - i);
+                }
             }
         }
 
-        return lionIndex != lionInfo.length;
+        scoreGap = lionScore - aPeachScore;
+
+        if (scoreGap > 0) {
+            if (maxGap < scoreGap) {
+                maxGap = scoreGap;
+                answer = lionInfo.clone();
+
+            } else if (maxGap == scoreGap) {
+                if (checkArr(answer, lionInfo)) {
+                    answer = lionInfo.clone();
+                }
+            }
+        }
+
+    }
+
+    public boolean checkArr(final int[] answer, final int[] lionInfo) {
+        for (int i = lionInfo.length - 1; i >= 0; i--) {
+            if (answer[i] == lionInfo[i]) {
+                continue;
+            }
+
+            return answer[i] <= lionInfo[i];
+        }
+
+        return false;
     }
     
 }
