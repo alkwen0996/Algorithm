8054d98575d96eba45aadd997cf5e963cddf1ee8
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-08-30 10:25:15 +0900

    [Gold V] Title: 신기한 소수, Time: 168 ms, Memory: 16164 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,42 +1,41 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    private static final String NEW_LINE = "\n";
-
-    private static final StringBuilder stringBuilder = new StringBuilder();
+    public static final String NEW_LINE = "\n";
 
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        final int inputNumberLength = Integer.parseInt(bufferedReader.readLine());
+        final int n = Integer.parseInt(bufferedReader.readLine());
+        final StringBuilder stringBuilder = new StringBuilder();
 
-        findInterestingPrimeNumber(inputNumberLength, 0, "");
+        findAmazingPrimeNumber(n, 0, "", stringBuilder);
         System.out.println(stringBuilder);
     }
 
-    private static void findInterestingPrimeNumber(final int inputNumber, int index, final String interestingPrimeNumber) {
-        if (index == inputNumber) {
-            stringBuilder.append(interestingPrimeNumber).append(NEW_LINE);
+    private static void findAmazingPrimeNumber(final int n, final int index, final String amazingNumber, final StringBuilder stringBuilder) {
+        if (index == n) {
+            stringBuilder.append(amazingNumber).append(NEW_LINE);
+
             return;
         }
 
         for (int i = 1; i <= 9; i++) {
-            if (isPrimeNumber(Integer.parseInt(interestingPrimeNumber + i))) {
-                findInterestingPrimeNumber(inputNumber, index + 1, interestingPrimeNumber + i);
+            if (checkPrimeNumber(Integer.parseInt(amazingNumber + i))) {
+                findAmazingPrimeNumber(n, index + 1, amazingNumber + i, stringBuilder);
             }
         }
-
     }
 
-    private static boolean isPrimeNumber(final int number) {
+
+    private static boolean checkPrimeNumber(final int number) {
         if (number == 1) {
             return false;
         }
 
-        for (int i = 2; i <= (int) Math.sqrt(number); i++) {
+        // 각 자리가 소수인지 확인
+        for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
                 return false;
             }
