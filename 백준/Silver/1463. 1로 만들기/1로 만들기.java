1688d02567d55319bc4785e0e928320d0a22d6e5
Jerome <34956859+alkwen0996@users.noreply.github.com>
2023-09-14 14:14:19 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,35 +1,40 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    private static int result = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
 
         int count = 0;
-        int result = createOne(n, count);
+        createOne(n, count);
 
         System.out.println(result);
     }
 
-    private static int createOne(int n, int count) {
-        int[] dp = new int[n + 1];
+    private static void createOne(int n, int count) {
+        if (n == 1) {
+            result = Math.min(count, result);
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0 ) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-
-            if (i % 3 == 0 ) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
+            return;
         }
 
-        return dp[n];
+        if (count >= result) {
+            return;
+        }
+
+        if (n % 3 == 0) {
+            createOne(n / 3, count + 1);
+        }
+
+        if (n % 2 == 0) {
+            createOne(n / 2, count + 1);
+        }
+
+        createOne(n - 1, count + 1);
     }
 
 }
