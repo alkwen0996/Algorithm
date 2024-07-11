f8a9bc05a52279c264641e30f330c332ffec7801
Jerome <34956859+alkwen0996@users.noreply.github.com>
2024-07-11 21:52:51 +0900

    [Silver I] Title: Z, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -4,45 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static final String NEW_LINE = "\n";
-    public static final String SPACE = " ";
 
     public static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
         int r = Integer.parseInt(stringTokenizer.nextToken());
         int c = Integer.parseInt(stringTokenizer.nextToken());
 
         count = 0;
-
-        find((int) Math.pow(2, N), r, c);
+        searchMap((int) Math.pow(2, n), r, c);
         System.out.println(count);
-    }
+    } // main
 
-    private static void find(final int size, final int r, final int c) {
+    private static void searchMap(final int size, final int row, final int column) {
         if (size == 1) {
             return;
         }
 
-        int half = size / 2;
-
-        if (r < half && c < half) {
-            find(size / 2, r, c);
-        } else if (r < half) {
-            count += (size * size / 4);
-            find(size / 2, r, c - (size / 2));
-        } else if (c < half) {
+        if (row < size / 2 && column < size / 2) {
+            searchMap(size / 2, row, column);
+        } else if (row < size / 2 && column >= size / 2) {
+            count += size * size / 4;
+            searchMap(size / 2, row, column - (size / 2));
+        } else if (row >= size / 2 && column < size / 2) {
             count += (size * size / 4) * 2;
-            find(size / 2, r - (size / 2), c);
+            searchMap(size / 2, row - (size / 2), column);
         } else {
             count += (size * size / 4) * 3;
-            find(size / 2, r - (size / 2), c - (size / 2));
+            searchMap(size / 2, row - (size / 2), column - (size / 2));
         }
-
     }
 
-}
+} // class
