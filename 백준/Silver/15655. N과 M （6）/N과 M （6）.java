710720c436d6a51aebc13cf039cc98ba91e6157e
Jerome <34956859+alkwen0996@users.noreply.github.com>
2024-07-31 11:24:07 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        final int n = Integer.parseInt(stringTokenizer.nextToken());
+        final int m = Integer.parseInt(stringTokenizer.nextToken());
+
+        final int[] input = new int[n];
+        final int[] permutation = new int[m];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(input);
+        generatePermutation(input, permutation, m, 0, 0);
+        System.out.println(stringBuilder);
+    }
+
+    private static void generatePermutation(final int[] input, final int[] permutation, final int m, final int count, final int flag) {
+        if (count == m) {
+            for (final int element : permutation) {
+                stringBuilder.append(element).append(" ");
+            }
+
+            stringBuilder.append("\n");
+
+            return;
+        }
+
+        for (int i = count; i < input.length; i++) {
+            if ((flag & 1 << i) != 0) {
+                continue;
+            }
+
+            if (count > 0) {
+                if (input[i] < permutation[count - 1]) {
+                    continue;
+                }
+            }
+
+            permutation[count] = input[i];
+            generatePermutation(input, permutation, m, count + 1, flag | 1 << i);
+        }
+
+    }
+
+}
