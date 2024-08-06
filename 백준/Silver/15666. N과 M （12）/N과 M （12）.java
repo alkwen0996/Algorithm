a25cc953655153350d43d726a0710862dd844cf7
Jerome <34956859+alkwen0996@users.noreply.github.com>
2024-08-06 18:27:23 +0900

    [Silver II] Title: N과 M (12), Time: 112 ms, Memory: 14592 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int[] numbers = new int[n];
+        int[] permutation = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        Arrays.sort(numbers);
+        createPermutation(numbers, permutation, 0, 0);
+        System.out.println(stringBuilder);
+    }
+
+    private static void createPermutation(final int[] numbers, final int[] permutation, final int step, final int start) {
+        if (step == permutation.length) {
+
+            for (int i = 0; i < permutation.length; i++) {
+                stringBuilder.append(permutation[i]).append(" ");
+            }
+
+            stringBuilder.append("\n");
+
+            return;
+        }
+
+        int prev = 0;
+        for (int i = start; i < numbers.length; i++) {
+            if (prev == numbers[i]) {
+                continue;
+            }
+
+            prev = numbers[i];
+            permutation[step] = numbers[i];
+            createPermutation(numbers, permutation, step + 1, i);
+        }
+    }
+
+} // class
