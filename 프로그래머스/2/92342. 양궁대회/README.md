c4f4da05b8feaa49c85de953816faa56a9ec812a
Jerome <34956859+alkwen0996@users.noreply.github.com>
2024-10-18 15:59:01 +0900

    [level 2] Title: 양궁대회, Time: 1.15 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/README.md
+++ 프로그래머스/2/92342. 양궁대회/README.md
@@ -1,10 +1,10 @@
 # [level 2] 양궁대회 - 92342 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92342#qna) 
 
 ### 성능 요약
 
-메모리: 90 MB, 시간: 8362.71 ms
+메모리: 77.5 MB, 시간: 1.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 15:25:56
+2024년 10월 18일 15:58:59
 
 ### 문제 설명
 
--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -7,7 +7,7 @@ class Solution {
         answer = new int[info.length];
         lionInfo = new int[info.length];
 
-        backTacking(n, info);
+        backTacking(n, info, 0);
 
         if (max == Integer.MIN_VALUE) {
             return new int[]{-1};
@@ -16,7 +16,7 @@ class Solution {
         return answer;
     }
     
-    public void backTacking(final int n, final int[] info) {
+    public void backTacking(final int n, final int[] info, final int index) {
         if (n == 0) {
             // 우승자 결정
             findWinner(info);
@@ -25,46 +25,51 @@ class Solution {
         }
 
         // 점수 선택
-        for (int i = 0; i < info.length && info[i] >= lionInfo[i]; i++) {
-            lionInfo[i]++;
-            backTacking(n - 1, info);
-            lionInfo[i]--;
+        for (int i = index; i < info.length; i++) {
+            int count = Math.min(n, info[i] + 1);
+
+            lionInfo[i] = count;
+            backTacking(n - count, info, i + 1);
+            lionInfo[i] = 0;
         }
     }
 
     public void findWinner(final int[] info) {
-        int lionScore = 0;
-        int apeachScore = 0;
-        int scoreDiff;
+        int scoreDiff = getScoreDiff(info);
 
-        for (int i = 0; i < info.length; i++) {
-            if (lionInfo[i] > info[i]) {
-                lionScore += 10 - i;
-            } else {
-                if (info[i] != 0) {
-                    if (lionInfo[i] <= info[i]) {
-                        apeachScore += 10 - i;
-                    }
-                }
-            }
-        }
-
-        scoreDiff = lionScore - apeachScore;
-
-        if (scoreDiff > 0) {
+        if(scoreDiff > 0){
             if (max < scoreDiff) {
                 max = scoreDiff;
                 answer = lionInfo.clone();
             } else if (max == scoreDiff) {
                 for (int i = lionInfo.length - 1; i >= 0; i--) {
-                    if (lionInfo[i] > answer[i]) {
-                        answer = lionInfo.clone();
+                    if(lionInfo[i] != answer[i]){
+                        if (lionInfo[i] > answer[i]) {
+                            answer = lionInfo.clone();
+                        }
 
                         break;
                     }
                 }
             }
         }
+    }
+    
+    public int getScoreDiff(final int[] info) {
+        int lionScore = 0;
+        int apeachScore = 0;
+
+        for (int i = 0; i < info.length; i++) {
+            if (lionInfo[i] + info[i] > 0) {
+                if (lionInfo[i] > info[i]) {
+                    lionScore += 10 - i;
+                } else if (lionInfo[i] <= info[i]) {
+                    apeachScore += 10 - i;
+                }
+            }
+        }
+
+        return lionScore - apeachScore;
     }
     
 }
